f3c5465d7ae58babbfef6a4cad3d32f2c201cd51
Soumadip <39thdecember@gmail.com>
2025-04-19 14:34:08 +0530

    Profile Page Structure ☑️

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { FormRow } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useOutletContext } from "react-router-dom";
+import { useNavigation, Form } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
 
 function Profile() {
+  const { currentUser } = useOutletContext();
+  const { name, lastName, email, location } = currentUser;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return (
-    <div>Profile</div>
-  )
+    <Wrapper>
+      <Form method="post" className="form" encType="multipart/form-data">
+        <h4 className="form-title">profile</h4>
+        <div className="form-center">
+          {/* file input 📂 */}
+          <div className="form-row">
+            <label htmlFor="avatar" className="form-label">
+              Select an image file (max 0.5 mb)
+            </label>
+            <input
+              type="file"
+              id="avatar"
+              className="form-input"
+              accept="image/*"
+            />
+          </div>
+          <FormRow type="text" name="name" defaultValue={name} />
+          <FormRow
+            type="text"
+            name="lastName"
+            defaultValue={lastName}
+            labelTxt="last name"
+          />
+          <FormRow type="email" name="email" defaultValue={email} />
+          <FormRow type="text" name="location" defaultValue={location} />
+          <button
+            className="btn btn-block form-btn"
+            type="submit"
+            disabled={isSubmitting}>
+            {isSubmitting ? "Submitting... ⌛" : "Submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 }
 
-export default Profile
+export default Profile;
